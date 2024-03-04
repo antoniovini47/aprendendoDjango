8097ad45fba9a0b40d7b2bc63e0c4c41ed33cee5
Antonio Oliveira <antoniovini47@gmail.com>
2024-03-03 23:12:38 -0300

    Primeiras views e funcionalidades

--- eleicoesEmDjango/meuSite/meuSite/settings.py
+++ eleicoesEmDjango/meuSite/meuSite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,11 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
-USE_I18N = True
+USE_I18N = False
 
 USE_TZ = True
 
--- eleicoesEmDjango/meuSite/polls/views.py
+++ eleicoesEmDjango/meuSite/polls/views.py
@@ -1,6 +1,39 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Olá mundo, você está no index das eleições.")
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.db.models import F
+from django.urls import reverse
+from .models import Question,Choice
 
 # Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+def vote(request, question_id):
+    question=get_object_or_404(Question, pk=question_id)
+    try:
+        selecte_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "Selecione alguma opção"
+            }
+        )
+    else:
+        selecte_choice.votes = F("votes") + 1
+        selecte_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
